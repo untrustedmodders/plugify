258c48045272d3a0c1e8a4e56ae2f942b1eb425b
Nikita Ushakov <ushakovn@tcd.ie>
2023-12-24 22:08:30 +0000

    Remove GetMethod and add anEndPlugin to ILanguageModule

--- include/wizard/language_module.h
+++ include/wizard/language_module.h
@@ -14,9 +14,9 @@ namespace wizard {
     public:
         virtual bool Initialize() = 0;
         virtual void Shutdown() = 0;
-        virtual void* GetMethod(const std::string& name) = 0;
         virtual void OnNativeAdded(/*data*/) = 0;
         virtual LoadResult OnLoadPlugin(const IPlugin& plugin) = 0;
         virtual void OnStartPlugin(const IPlugin& plugin) = 0;
+        virtual void OnEndPlugin(const IPlugin& plugin) = 0;
     };
 }
