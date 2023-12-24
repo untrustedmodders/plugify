2524593c5f7d3f31600c6ed0e4dcd6f75151f3b3
Nikita Ushakov <ushakovn@tcd.ie>
2023-12-24 21:17:40 +0000

    Fix typo

--- src/utils/paths.h
+++ src/utils/paths.h
@@ -9,7 +9,7 @@ namespace wizard {
         static const fs::path& PluginsDir();
     };
 
-    // std::filesystem::hash_value not in the C++20 standard by default
+    // std::hash<std::filesystem::path> not in the C++20 standard by default
     struct PathHash {
         auto operator()(const fs::path& path) const noexcept {
             return fs::hash_value(path);
