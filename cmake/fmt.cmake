4058b707d3f6468b592e23f8d0825bcf7267579d
Nikita Ushakov <ushakovn@tcd.ie>
2024-02-20 21:40:28 +0000

    make fmt header only

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -8,4 +8,10 @@ FetchContent_Declare(
         GIT_TAG 10.2.1
 )
 
+set(FMT_DOC OFF CACHE INTERNAL "Generate the doc target.")
+set(FMT_INSTALL OFF CACHE INTERNAL "Generate the install target.")
+set(FMT_TEST OFF CACHE INTERNAL "Generate the test target.")
+set(FMT_FUZZ OFF CACHE INTERNAL "Generate the fuzz target.")
+set(FMT_CUDA_TEST OFF CACHE INTERNAL "Generate the cuda-test target.")
+
 FetchContent_MakeAvailable(fmt)
