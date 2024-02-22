1a0ec5d082a73c1c9514bfb5d7d7ec031fa23a0e
Nikita Ushakov <ushakovn@tcd.ie>
2024-02-22 20:09:51 +0000

    Remove unused ctor

--- src/utils/library_search_dirs.h
+++ src/utils/library_search_dirs.h
@@ -3,7 +3,6 @@
 namespace plugify {
 	class LibrarySearchDirs {
 	public:
-		LibrarySearchDirs() = default;
 		virtual ~LibrarySearchDirs() = 0;
 
 		static std::unique_ptr<LibrarySearchDirs> Add(const std::vector<fs::path>& additionalSearchDirectories);
