5db344502eb7076b9de2e1453efb1d703c517844
Nikita Ushakov <ushakovn@tcd.ie>
2024-02-29 21:28:43 +0000

    Fix plugin schema

--- include/plugify/method.h
+++ include/plugify/method.h
@@ -48,6 +48,20 @@ namespace plugify {
 		ArrayFloat,
 		ArrayDouble,
 		ArrayString,
+		// glm:mat
+		//Mat2x2,
+		//Mat2x3,
+		//Mat2x4,
+		//Mat3x2,
+		//Mat3x3,
+		//Mat3x4,
+		//Mat4x2,
+		//Mat4x3,
+		//Mat4x4,
+		// glm:vec
+		Vec2,
+		Vec3,
+		Vec4,
 
 		LastPrimitive = Function
 	};
