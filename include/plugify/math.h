834d2b679a71232b796e19d9d51ca90359a67438
Nikita Ushakov <ushakovn@tcd.ie>
2024-03-05 02:28:48 +0000

    Add unions to matrix

--- include/plugify/math.h
+++ include/plugify/math.h
@@ -1,13 +1,20 @@
 #pragma once
 
+#include <array>
+
 namespace plugify {
 	/**
 	 * @struct Vector2
 	 * @brief Represents a 2D vector with x and y components.
 	 */
 	struct Vector2 {
-		float x{}; /**< x-component of the vector. */
-		float y{}; /**< y-component of the vector. */
+		union {
+			struct {
+				float x; ///< x-component of the vector.
+				float y; ///< y-component of the vector.
+			};
+			std::array<float, 2> data{}; ///< Array representation of the vector.
+		};
 
 		/**
 		 * @brief Assignment operator for Vector2.
@@ -22,9 +29,14 @@ namespace plugify {
 	 * @brief Represents a 3D vector with x, y, and z components.
 	 */
 	struct Vector3 {
-		float x{}; /**< x-component of the vector. */
-		float y{}; /**< y-component of the vector. */
-		float z{}; /**< z-component of the vector. */
+		union {
+			struct {
+				float x; ///< x-component of the vector.
+				float y; ///< y-component of the vector.
+				float z; ///< z-component of the vector.
+			};
+			std::array<float, 3> data{}; ///< Array representation of the vector.
+		};
 
 		/**
 		 * @brief Assignment operator for Vector3.
@@ -39,10 +51,15 @@ namespace plugify {
 	 * @brief Represents a 4D vector with x, y, z, and w components.
 	 */
 	struct Vector4 {
-		float x{}; /**< x-component of the vector. */
-		float y{}; /**< y-component of the vector. */
-		float z{}; /**< z-component of the vector. */
-		float w{}; /**< w-component of the vector. */
+		union {
+			struct {
+				float x; ///< x-component of the vector.
+				float y; ///< y-component of the vector.
+				float z; ///< z-component of the vector.
+				float w; ///< w-component of the vector.
+			};
+			std::array<float, 4> data{}; ///< Array representation of the vector.
+		};
 
 		/**
 		 * @brief Assignment operator for Vector4.
@@ -57,7 +74,24 @@ namespace plugify {
 	 * @brief Represents a 4x4 matrix using Vector4 rows.
 	 */
 	struct Matrix4x4 {
-		Vector4 rows[4]{}; /**< Rows of the matrix represented by Vector4. */
+		union {
+			struct {
+				float m11, m12, m13, m14;
+				float m21, m22, m23, m24;
+				float m31, m32, m33, m34;
+				float m41, m42, m43, m44;
+			};
+			Vector4 rows[4]; ///< Rows of the matrix represented by Vector4.
+			std::array<float, 16> data; ///< Array representation of the matrix.
+		};
+
+		/**
+		 * @brief Default constructor. Initializes the matrix to the identity matrix.
+		 */
+		Matrix4x4() : m11{1.0f}, m12{0.0f}, m13{0.0f}, m14{0.0f},
+					  m21{0.0f}, m22{1.0f}, m23{0.0f}, m24{0.0f},
+					  m31{0.0f}, m32{0.0f}, m33{1.0f}, m34{0.0f},
+					  m41{0.0f}, m42{0.0f}, m43{0.0f}, m44{1.0f} {}
 
 		/**
 		 * @brief Assignment operator for Matrix4x4.
@@ -72,7 +106,22 @@ namespace plugify {
 	 * @brief Represents a 3x2 matrix using Vector3 rows.
 	 */
 	struct Matrix3x2 {
-		Vector3 rows[2]{}; /**< Rows of the matrix represented by Vector3. */
+		union {
+			struct {
+				float m11, m12;
+				float m21, m22;
+				float m31, m32;
+			};
+			Vector2 rows[3]; ///< Rows of the matrix represented by Vector3.
+			std::array<float, 6> data; ///< Array representation of the matrix.
+		};
+
+		/**
+		 * @brief Default constructor. Initializes the matrix to the identity matrix.
+		 */
+		Matrix3x2() : m11{1.0f}, m12{0.0f},
+					  m21{0.0f}, m22{1.0f},
+					  m31{0.0f}, m32{0.0f} {}
 
 		/**
 		 * @brief Assignment operator for Matrix3x2.
