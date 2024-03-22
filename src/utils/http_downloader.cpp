3bd71aa67302c5fc154de52603da65aaadd17bf9
Nikita Ushakov <ushakovn@tcd.ie>
2024-03-22 00:14:53 +0000

    Change url regex

--- src/utils/http_downloader.cpp
+++ src/utils/http_downloader.cpp
@@ -265,6 +265,6 @@ std::string_view HTTPDownloader::GetExtensionForContentType(std::string_view con
 }
 
 bool HTTPDownloader::IsValidURL(const std::string& url) {
-	static std::regex regex(R"(^(?:(?:https?|ftp)://)(?:\S+@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:[a-z\u00a1-\uffff0-9]+-)*[a-z\u00a1-\uffff0-9]+(?:\.(?:[a-z\u00a1-\uffff0-9]+-)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:/\S*)?$)");
+	static std::regex regex(R"(^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$)");
 	return !url.empty() && std::regex_match(url, regex);
 }
