e6806eb9993779ed421fa7106ebfd7fc7e7c9a08
qubka <ushakovn@tcd.ie>
2024-09-22 15:16:49 +0100

    Add to_string implementation to string

--- include/plugify/string.h
+++ include/plugify/string.h
@@ -17,6 +17,7 @@
 #include <compare>           // for std::strong_ordering
 #include <memory>            // for std::allocator, std::swap, std::allocator_traits
 #include <limits>            // for std::numeric_limits
+#include <charconv>          // for std::to_chars
 
 #include <cstdint>
 #include <cstddef>
@@ -1733,96 +1734,70 @@ namespace plg {
 #endif
 
 	namespace detail {
-		template<typename Type>
-		constexpr std::size_t to_chars_len(Type value) {
-			constexpr Type b1 = 10;
-			constexpr Type b2 = 100;
-			constexpr Type b3 = 1000;
-			constexpr Type b4 = 10000;
+		template<typename S, typename V>
+		_PLUGIFY_ALWAYS_INLINE S to_string(V v) {
+			//  numeric_limits::digits10 returns value less on 1 than desired for unsigned numbers.
+			//  For example, for 1-byte unsigned value digits10 is 2 (999 can not be represented),
+			//  so we need +1 here.
+			constexpr size_t bufsize = std::numeric_limits<V>::digits10 + 2; // +1 for minus, +1 for digits10
+			char buf[bufsize];
+			const auto res = std::to_chars(buf, buf + bufsize, v);
+			return S(buf, res.ptr);
+		}
 
-			for (std::size_t i = 1;; i += 4, value /= b4) {
-				if (value < b1)
-					return i;
-				if (value < b2)
-					return i + 1;
-				if (value < b3)
-					return i + 2;
-				if (value < b4)
-					return i + 3;
+		template<typename S, typename V>
+		_PLUGIFY_ALWAYS_INLINE S as_string(const typename S::value_type* fmt, V v) {
+			typedef typename S::size_type size_type;
+			S s;
+			s.resize(s.capacity());
+			size_type available = s.size();
+			while (true) {
+				int status = std::swprintf(&s[0], available + 1, fmt, v);
+				if (status >= 0) {
+					auto used = static_cast<size_type>(status);
+					if (used <= available) {
+						s.resize(used);
+						break;
+					}
+					available = used; // Assume this is advice of how much space we need.
+				} else {
+					available = available * 2 + 1;
+				}
+				s.resize(available);
 			}
-		}
-
-		static constexpr char digits[201] =
-				"0001020304050607080910111213141516171819"
-				"2021222324252627282930313233343536373839"
-				"4041424344454647484950515253545556575859"
-				"6061626364656667686970717273747576777879"
-				"8081828384858687888990919293949596979899";
-
-		constexpr void to_chars(char* first, std::size_t len, auto val) {
-			std::size_t pos = len - 1;
-			while (val >= 100) {
-				auto const num = (val % 100) * 2;
-				val /= 100;
-				first[pos] = digits[num + 1];
-				first[pos - 1] = digits[num];
-				pos -= 2;
-			}
-			if (val >= 10) {
-				auto const num = val * 2;
-				first[1] = digits[num + 1];
-				first[0] = digits[num];
-			} else
-				first[0] = '0' + static_cast<char>(val);
-		}
-
-		template<std::signed_integral Type, std::unsigned_integral UType = std::make_unsigned_t<Type>>
-		constexpr _PLUGIFY_ALWAYS_INLINE string to_string(Type value) {
-			const auto negative = value < 0;
-			const UType uvalue = negative ? static_cast<UType>(~value) + static_cast<UType>(1) : static_cast<UType>(value);
-			const auto length = to_chars_len(uvalue);
-			string str(length + negative, '-');
-			to_chars(&str[negative], length, uvalue);
-			return str;
-		}
-
-		template<std::unsigned_integral Type>
-		constexpr _PLUGIFY_ALWAYS_INLINE string to_string(Type value) {
-			string str(to_chars_len(value), '\0');
-			to_chars(&str[0], str.length(), value);
-			return str;
+			return s;
 		}
 	}// namespace detail
 
-	constexpr inline string to_string(int val) { return detail::to_string(val); }
-	constexpr inline string to_string(unsigned val) { return detail::to_string(val); }
-	constexpr inline string to_string(long val) { return detail::to_string(val); }
-	constexpr inline string to_string(unsigned long val) { return detail::to_string(val); }
-	constexpr inline string to_string(long long val) { return detail::to_string(val); }
-	constexpr inline string to_string(unsigned long long val) { return detail::to_string(val); }
+	constexpr string to_string(int val) { return detail::to_string<string>(val); }
+	constexpr string to_string(unsigned val) { return detail::to_string<string>(val); }
+	constexpr string to_string(long val) { return detail::to_string<string>(val); }
+	constexpr string to_string(unsigned long val) { return detail::to_string<string>(val); }
+	constexpr string to_string(long long val) { return detail::to_string<string>(val); }
+	constexpr string to_string(unsigned long long val) { return detail::to_string<string>(val); }
 
 #if PLUGIFY_STRING_FLOAT
-	// constexpr inline string to_string(float val);
-	// constexpr inline string to_string(double val);
+	constexpr string to_string(float val) { return detail::as_string<string>("%f", val); }
+	constexpr string to_string(double val) { return detail::as_string<string>("%f", val); }
 
 #if PLUGIFY_STRING_LONG_DOUBLE
-	// constexpr inline string to_string(long double val);
+	constexpr string to_string(long double val) { return detail::as_string<string>("%Lf", val); }
 #endif
 #endif
 
-	// constexpr inline wstring to_wstring(int val);
-	// constexpr inline wstring to_wstring(unsigned val);
-	// constexpr inline wstring to_wstring(long val);
-	// constexpr inline wstring to_wstring(unsigned long val);
-	// constexpr inline wstring to_wstring(long long val);
-	// constexpr inline wstring to_wstring(unsigned long long val);
+	constexpr wstring to_wstring(int val) { return detail::to_string<wstring>(val); }
+	constexpr wstring to_wstring(unsigned val) { return detail::to_string<wstring>(val); }
+	constexpr wstring to_wstring(long val) { return detail::to_string<wstring>(val); }
+	constexpr wstring to_wstring(unsigned long val) { return detail::to_string<wstring>(val); }
+	constexpr wstring to_wstring(long long val) { return detail::to_string<wstring>(val); }
+	constexpr wstring to_wstring(unsigned long long val) { return detail::to_string<wstring>(val); }
 
 #if PLUGIFY_STRING_FLOAT
-	// constexpr inline wstring to_wstring(float val);
-	// constexpr inline wstring to_wstring(double val);
+	constexpr wstring to_wstring(float val) { return detail::as_string<wstring>(L"%f", val); }
+	constexpr wstring to_wstring(double val) { return detail::as_string<wstring>(L"%f", val); }
 
 #if PLUGIFY_STRING_LONG_DOUBLE
-	// constexpr inline wstring to_wstring(long double val);
+	constexpr wstring to_wstring(long double val) { return detail::as_string<wstring>(L"%Lf", val); }
 #endif
 #endif
 #endif
@@ -1879,11 +1854,11 @@ namespace plg {
 			_PLUGIFY_STRING_DIAG_IGN(4455)
 #endif
 			// suffix for basic_string literals
-			constexpr inline string operator""s(const char* str, std::size_t len) { return string{str, len}; }
-			constexpr inline u8string operator""s(const char8_t* str, std::size_t len) { return u8string{str, len}; }
-			constexpr inline u16string operator""s(const char16_t* str, std::size_t len) { return u16string{str, len}; }
-			constexpr inline u32string operator""s(const char32_t* str, std::size_t len) { return u32string{str, len}; }
-			constexpr inline wstring operator""s(const wchar_t* str, std::size_t len) { return wstring{str, len}; }
+			constexpr string operator""s(const char* str, std::size_t len) { return string{str, len}; }
+			constexpr u8string operator""s(const char8_t* str, std::size_t len) { return u8string{str, len}; }
+			constexpr u16string operator""s(const char16_t* str, std::size_t len) { return u16string{str, len}; }
+			constexpr u32string operator""s(const char32_t* str, std::size_t len) { return u32string{str, len}; }
+			constexpr wstring operator""s(const wchar_t* str, std::size_t len) { return wstring{str, len}; }
 
 			_PLUGIFY_STRING_DIAG_POP()
 		}// namespace string_literals
@@ -1911,13 +1886,13 @@ namespace std {
 #endif
 
 #if PLUGIFY_STRING_STD_FORMAT
-// hash support
+// format support
 #ifdef FMT_HEADER_ONLY
 namespace fmt {
 #else
 namespace std {
 #endif
-	/*template<typename Allocator>
+	template<typename Allocator>
 	struct formatter<plg::basic_string<char, std::char_traits<char>, Allocator>> : plg::detail::string_formatter_base<char, Allocator> {};
 
 	template<typename Allocator>
@@ -1930,18 +1905,6 @@ namespace std {
 	struct formatter<plg::basic_string<char32_t, std::char_traits<char32_t>, Allocator>> : plg::detail::string_formatter_base<char32_t, Allocator> {};
 
 	template<typename Allocator>
-	struct formatter<plg::basic_string<wchar_t, std::char_traits<wchar_t>, Allocator>> : plg::detail::string_formatter_base<wchar_t, Allocator> {};*/
+	struct formatter<plg::basic_string<wchar_t, std::char_traits<wchar_t>, Allocator>> : plg::detail::string_formatter_base<wchar_t, Allocator> {};
 }// namespace std
-
-
-template<>
-struct std::formatter<plg::string> {
-    constexpr auto parse(std::format_parse_context& ctx) {
-        return ctx.begin();
-    }
-
-    auto format(const plg::string& str, std::format_context& ctx) const {
-        return std::format_to(ctx.out(), "{}", str.c_str());
-    }
-};
 #endif
