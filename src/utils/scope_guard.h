9b524f608c1bdfaa257a3dd01f40c6ce7bd78ecf
qubka <ushakovn@tcd.ie>
2024-10-13 18:14:28 +0100

    Add scope dir

--- src/utils/scope_guard.h
+++ src/utils/scope_guard.h
@@ -1,27 +1,26 @@
 #pragma once
 
+#include <functional>
+#include <type_traits>
+
 namespace plugify {
 	class ScopeGuard {
 	public:
-		template<class Callable> requires !std::is_same_v<Callable, ScopeGuard>
-		explicit ScopeGuard(Callable&& undo) : f(std::forward<Callable>(undo)) {}
-		ScopeGuard(ScopeGuard&& other) noexcept : f(std::move(other.f)) { other.f = nullptr; }
 		ScopeGuard() : f(nullptr) {}
 		~ScopeGuard() { if (f) f(); }
 		ScopeGuard(const ScopeGuard&) = delete;
+		template<class Callable> requires (!std::is_same_v<Callable, ScopeGuard>)
+		explicit ScopeGuard(Callable&& undo) : f(std::forward<Callable>(undo)) {}
+		ScopeGuard(ScopeGuard&& other) noexcept : f(std::move(other.f)) { other.f = nullptr; }
 
-		ScopeGuard& operator=(const ScopeGuard&) & = default;
-		ScopeGuard& operator=(const ScopeGuard&) && = delete;
-
+		ScopeGuard& operator=(const ScopeGuard&) = delete;
 		ScopeGuard& operator=(ScopeGuard&& other) noexcept {
 			if (f) f();
 			f = std::move(other.f);
 			other.f = nullptr;
 			return *this;
 		}
-		ScopeGuard& operator=(ScopeGuard&&) && = delete;
-
-		template<class Callable> requires !std::is_same_v<Callable, ScopeGuard>
+		template<class Callable> requires (!std::is_same_v<Callable, ScopeGuard>)
 		ScopeGuard& operator=(Callable&& undo) {
 			if (f) f();
 			f = std::forward<Callable>(undo);
