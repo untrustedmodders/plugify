437b1a47303885ffa0b30b40402fbb7293b96987
qubka <ushakovn@tcd.ie>
2025-03-30 21:02:51 +0100

    fix: linux logger

--- test/plug/std_logger.cpp
+++ test/plug/std_logger.cpp
@@ -8,28 +8,28 @@ namespace plug {
 		if (severity <= _severity) {
 			switch (severity) {
 				case plugify::Severity::None:
-					std::osyncstream{std::cout} << message << std::endl;
+					std::cout << message << std::endl;
 					break;
 				case plugify::Severity::Fatal:
-					std::osyncstream{std::cerr} << "[@] Fatal: " << message << std::endl;
+					std::cerr << "[@] Fatal: " << message << std::endl;
 					break;
 				case plugify::Severity::Error:
-					std::osyncstream{std::cerr} << "[#] Error: " << message << std::endl;
+					std::cerr << "[#] Error: " << message << std::endl;
 					break;
 				case plugify::Severity::Warning:
-					std::osyncstream{std::cout} << "[!] Warning: " << message << std::endl;
+					std::cout << "[!] Warning: " << message << std::endl;
 					break;
 				case plugify::Severity::Info:
-					std::osyncstream{std::cout} << "[+] Info: " << message << std::endl;
+					std::cout << "[+] Info: " << message << std::endl;
 					break;
 				case plugify::Severity::Debug:
-					std::osyncstream{std::cout} << "[~] Debug: " << message << std::endl;
+					std::cout << "[~] Debug: " << message << std::endl;
 					break;
 				case plugify::Severity::Verbose:
-					std::osyncstream{std::cout} << "[*] Verbose: " << message << std::endl;
+					std::cout << "[*] Verbose: " << message << std::endl;
 					break;
 				default:
-					std::osyncstream{std::cerr} << "Unsupported error message logged " << message << std::endl;
+					std::cerr << "Unsupported error message logged " << message << std::endl;
 					break;
 			}
 		}
