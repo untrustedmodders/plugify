e1ba267c9c551a16fa1d45697968a8de134b55f2
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2025-04-01 23:14:13 +0100

    fix: add miniz patch

--- cmake/FetchMiniz.cmake
+++ cmake/FetchMiniz.cmake
@@ -6,6 +6,7 @@ FetchContent_Declare(
         miniz
         GIT_REPOSITORY https://github.com/richgel999/miniz.git
         GIT_TAG 3.0.2
+        PATCH_COMMAND git apply ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/miniz.patch
 )
 
 FetchContent_MakeAvailable(miniz)
