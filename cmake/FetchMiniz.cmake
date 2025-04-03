4d4398325a931794ffdba599b9bb9daae7ca623a
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2025-04-03 23:18:59 +0100

    fix: remove miniz patch

--- cmake/FetchMiniz.cmake
+++ cmake/FetchMiniz.cmake
@@ -6,7 +6,6 @@ FetchContent_Declare(
         miniz
         GIT_REPOSITORY ${PLUGIFY_MINIZ_REPO}
         GIT_TAG ${PLUGIFY_MINIZ_TAG}
-        PATCH_COMMAND git reset --hard && git clean -f -d && git apply ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/miniz.patch
 )
 
 FetchContent_MakeAvailable(miniz)
