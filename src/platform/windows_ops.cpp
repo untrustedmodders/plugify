c99398a7a8edd18c7c3ca32cd773ea8c62adb6a9
qubka <nikita.ushakov0000@outlook.com>
2025-09-19 00:24:21 +0100

    fix: win build

--- src/platform/windows_ops.cpp
+++ src/platform/windows_ops.cpp
@@ -1,6 +1,12 @@
-#include <windows.h>
-
 #include "plugify/platform_ops.hpp"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#  define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#  define NOMINMAX
+#endif
+#include <windows.h>
 #undef LoadLibrary
 
 namespace plugify {
