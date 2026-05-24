8d5e25ed598a6e670d436350c746b04a07b1f54f
qubka <nikita.ushakov0000@outlook.com>
2026-05-24 13:45:43 +0100

    fix: update docs and improve of ILanguageModule

--- include/plugify/language_module.hpp
+++ include/plugify/language_module.hpp
@@ -39,8 +39,8 @@ namespace plugify {
 	 * @class ILanguageModule
 	 * @brief Interface for user-implemented language modules.
 	 *
-	 * The ILanguageModule interface defines methods that should be implemented by user-written
-	 * language modules.
+	 * The ILanguageModule interface defines methods that should be implemented
+	 * by user-written language modules.
 	 */
 	class ILanguageModule {
 	protected:
@@ -51,67 +51,63 @@ namespace plugify {
 		 * @brief Initialize the language module.
 		 * @param provider Weak ptr to the Plugify provider.
 		 * @param module Ref to the language module being initialized.
-		 * @return Result of the initialization, either InitResultData or string.
+		 * @return Result of the initialization, either InitData or an error string.
 		 */
 		virtual Result<InitData> Initialize(const Provider& provider, const Extension& module) = 0;
 
 		/**
 		 * @brief Shutdown the language module.
-		 * @return Result of the shutdown, either void or string.
+		 * @return Result of the shutdown, either void or an error string.
 		 */
 		virtual Result<void> Shutdown() = 0;
 
 		/**
 		 * @brief Handle actions to be performed on each frame.
 		 * @param deltaTime The time delta since the last update.
+		 * @return Result of the update, either void or an error string.
 		 */
 		virtual Result<void> OnUpdate(std::chrono::milliseconds deltaTime) = 0;
 
 		/**
-		 * @brief
-		 */
-		// virtual Result<void> OnPluginInitialize(const Extension& plugin) = 0;
-
-		/**
 		 * @brief Handle plugin load event.
-		 * @param plugin Ref to the loaded plugin.
-		 * @return Result of the load event, either LoadResultData or string.
+		 * @param plugin Ref to the plugin being loaded.
+		 * @return Result of the load event, either LoadData or an error string.
 		 */
 		virtual Result<LoadData> OnPluginLoad(const Extension& plugin) = 0;
 
 		/**
 		 * @brief Handle plugin start event.
-		 * @param plugin Ref to the started plugin.
-		 * @return Result of the start, either void or string.
+		 * @param plugin Ref to the plugin being started.
+		 * @return Result of the start, either void or an error string.
 		 */
 		virtual Result<void> OnPluginStart(const Extension& plugin) = 0;
 
 		/**
 		 * @brief Handle plugin update event.
-		 * @param plugin Ref to the started plugin.
+		 * @param plugin Ref to the plugin being updated.
 		 * @param deltaTime The time delta since the last update.
-		 * @return Result of the update, either void or string.
+		 * @return Result of the update, either void or an error string.
 		 */
 		virtual Result<void> OnPluginUpdate(const Extension& plugin, std::chrono::milliseconds deltaTime) = 0;
 
 		/**
 		 * @brief Handle plugin end event.
-		 * @param plugin Ref to the ended plugin.
-		 * @return Result of the end, either void or string.
+		 * @param plugin Ref to the plugin being ended.
+		 * @return Result of the end, either void or an error string.
 		 */
 		virtual Result<void> OnPluginEnd(const Extension& plugin) = 0;
 
 		/**
 		 * @brief Handle method export event.
 		 * @param plugin Ref to the plugin exporting a method.
-		 * @return Result of the export, either void or string.
+		 * @return Result of the export, either void or an error string.
 		 */
 		virtual Result<void> OnMethodExport(const Extension& plugin) = 0;
 
 		/**
-		 * @brief Determine if language module is build with debugging mode.
-		 * @return True if the assembly is build with debugging, false otherwise.
+		 * @brief Determine if the language module was built in debug mode.
+		 * @return True if built with debugging enabled, false otherwise.
 		 */
-		virtual bool IsDebugBuild() = 0;
+		virtual bool IsDebugBuild() const noexcept = 0;
 	};
 }  // namespace plugify
