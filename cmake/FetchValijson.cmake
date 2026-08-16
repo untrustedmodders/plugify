ad26781abff28830c2bfdb86d7a3a247f00213dc
qubka <ushakovn@tcd.ie>
2026-08-16 21:28:04 +0100

    fix: tmp clang build

--- cmake/FetchValijson.cmake
+++ cmake/FetchValijson.cmake
@@ -7,7 +7,10 @@ FetchContent_Declare(
         GIT_REPOSITORY ${PLUGIFY_VALIJSON_REPO}
         GIT_TAG ${PLUGIFY_VALIJSON_TAG}
         GIT_SHALLOW TRUE
+        GIT_PROGRESS TRUE
+        PATCH_COMMAND git reset --hard && git apply --ignore-whitespace --whitespace=nowarn --reject ${CMAKE_CURRENT_SOURCE_DIR}/patches/valijson.patch
         GIT_SUBMODULES ""
+        EXCLUDE_FROM_ALL
 )
 
 set(valijson_BUILD_TESTS OFF CACHE BOOL "Don't build valijson tests" FORCE)
